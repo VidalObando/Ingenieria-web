4c2129b83c015c4b54a2a1d90fe99db61f75e9c0
VidalObando <vidalobando@cotecnova.edu.co>
2025-02-22 13:25:57 -0500

    Se agregaron los demas resultados de las operaciones de acuerdo a la operación correspondiente

--- cliente.php
+++ cliente.php
@@ -1,3 +1,19 @@
 <?php
-	
+
+$options = array(
+    'location' => 'http://localhost/webservices/appwebservices/server.php',
+    'uri' => 'http://localhost/webservices/appwebservices/'
+);
+
+$client = new SoapClient (NULL, $options);
+
+$nombre = "Vidal Obando";
+
+echo $client->saludar ($nombre. '!!'). "</br>";
+
+echo "El resultado de la suma es: " . $client->operacion(10, 5, 'suma'). "</br>";
+echo "El resultado de la resta es: " . $client->operacion(10, 5, 'resta'). "</br>";
+echo "El resultado de la multiplicación es: " . $client->operacion(10, 5, 'multiplicacion'). "</br>";
+echo "El resultado de la división es: " . $client->operacion(10, 5, 'division'). "</br>";
+
 ?>
