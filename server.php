86ea23116c49358770a4fa70bfc5903f3ebf5a58
VidalObando <vidalobando@cotecnova.edu.co>
2025-02-22 16:12:25 -0500

    Se le agrega ahora el validarUsuario() verifica si un usuario y su clave existen en la base de datos. Toma estos dos valores, usuario y clave, y busca una coincidencia en la tabla usuarios. Si encuentra un registro con esos datos, devuelve el mensaje “Los datos ingresados son válidos”; de lo contrario, indica que los datos no coinciden.

--- server.php
+++ server.php
@@ -24,18 +24,44 @@ class serverSoap extends Conexion
                 return 'Operador no válido';
         }
     }
+
+    public function getProduct() 
+    {
+        $query = "SELECT * FROM producto";
+        $result = mysqli_query($this->db, $query);
+        
+        while ($row = mysqli_fetch_assoc($result))
+        {
+            return $row['nombre'];
+        }
+        $result->close();
+    }
+
+    public function validarUsuario($usuario, $clave)
+    {
+        $query = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND clave = '$clave'";
+        $result = mysqli_query($this->db, $query);
+        
+        if (mysqli_num_rows($result) > 0) 
+        {
+            return "Los datos ingresados son válidos";
+        } 
+        else 
+        {
+            return "Los datos ingresados no coinciden, intente de nuevo";
+        }
+    }
 }
 
 // Crear un nuevo servidor SOAP
 $options = array('uri' => 'http://localhost/webservices/appwebservices/');
 
 // Instanciar el servidor SOAP
-$server = new SoapServer (NULL, $options);
+$server = new SoapServer(NULL, $options);
 
 // Configurar qué clase manejará las solicitudes SOAP
 $server->setClass('serverSoap');
 
 // Procesar las solicitudes SOAP
-$server->handle ();
-
+$server->handle();
 ?>
