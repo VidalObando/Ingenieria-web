d32ca8dcf210c169dfc7a6cc837e9b299338239a
VidalObando <vidalobando@cotecnova.edu.co>
2025-02-22 13:15:28 -0500

    Se modificó la public function operacion ya sean (suma, resta, multiplicacion o division) Se utilizó un switch para determinar la operación a ejecutar y retorna el resultado correspondiente. De acuerdo al nombre del caso, este realizará la operacion correspondiente. Todo dentro de la misma funcion

--- server.php
+++ server.php
@@ -1,3 +1,41 @@
 <?php
-	
+
+require('conexion.php');
+
+class serverSoap extends Conexion
+{
+    public function saludar ($name)
+    {
+        return 'Hola, '.$name.'!';
+    }
+
+    public function operacion($num1, $num2, $operador)
+    {
+        switch ($operador) {
+            case 'suma':
+                return $num1 + $num2;
+            case 'resta':
+                return $num1 - $num2;
+            case 'multiplicacion':
+                return $num1 * $num2;
+            case 'division':
+                return ($num2 != 0) ? $num1 / $num2 : 'División por cero no permitida';
+            default:
+                return 'Operador no válido';
+        }
+    }
+}
+
+// Crear un nuevo servidor SOAP
+$options = array('uri' => 'http://localhost/webservices/appwebservices/');
+
+// Instanciar el servidor SOAP
+$server = new SoapServer (NULL, $options);
+
+// Configurar qué clase manejará las solicitudes SOAP
+$server->setClass('serverSoap');
+
+// Procesar las solicitudes SOAP
+$server->handle ();
+
 ?>
